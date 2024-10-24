9038123b34fab93176656d0fb22780d807301084
Dean Lofts <dean@deanlofts.xyz>
2024-10-24 21:12:48 +1100

    github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://loftwah.github.io/blog', // Adjust this to match your GitHub Pages URL
+  integrations: [mdx(), sitemap()],
 });
