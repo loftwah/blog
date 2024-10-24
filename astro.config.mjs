b7fdbfdbda7f3adde47368c02910162bf8e00f5a
Dean Lofts <dean@deanlofts.xyz>
2024-10-24 19:32:34 +1100

    step 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import compress from 'astro-compress';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://blog.deanlofts.xyz',
+  integrations: [
+    mdx(), 
+    sitemap(),
+    compress()  // for optimizing performance
+  ],
+  build: {
+    format: 'directory'  // Required for GitHub Pages
+  },
+  output: 'static', // Make sure the site builds as static for GitHub Pages
+  trailingSlash: 'always', // Use trailing slash to avoid issues with directories
 });
