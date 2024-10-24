ac327d1b41d3fb2b14b70d63f12fa43c6f6a2682
Dean Lofts <dean@deanlofts.xyz>
2024-10-24 21:24:10 +1100

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://loftwah.github.io/blog', // Adjust this to match your GitHub Pages URL
+  site: 'https://loftwah.github.io/blog',  // Make sure the site URL is correct for GitHub Pages
+  base: '/blog/',  // This tells Astro to serve the site from the /blog subdirectory
   integrations: [mdx(), sitemap()],
 });
