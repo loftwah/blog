133f4b0b2a595485be0c1f2aa84e90fc013c56d4
Dean Lofts <dean@deanlofts.xyz>
2024-10-24 21:43:59 +1100

    custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://loftwah.github.io/blog',  // Make sure the site URL is correct for GitHub Pages
-  base: '/blog/',  // This tells Astro to serve the site from the /blog subdirectory
+  site: 'https://blog.deanlofts.xyz',  // Your custom domain here
   integrations: [mdx(), sitemap()],
 });
