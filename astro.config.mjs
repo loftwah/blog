07f7dad865f88ff067578790374bb024fe15a38b
Dean Lofts <dean@deanlofts.xyz>
2025-03-15 16:09:33 +1100

    upgrade tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,19 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 import { addCopyButton } from 'shiki-transformer-copy-button';
 
 export default defineConfig({
   site: 'https://blog.deanlofts.xyz',
-  integrations: [mdx(), sitemap(), react(), tailwind()],
+  integrations: [
+    mdx(), 
+    sitemap(), 
+    react()
+  ],
+  vite: {
+    plugins: [tailwindcss()]
+  },
   markdown: {
     shikiConfig: {
       theme: 'nord', // Use your preferred theme
